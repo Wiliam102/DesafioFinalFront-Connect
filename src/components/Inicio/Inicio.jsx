50c6181466aa647994556a3ca26a41e89931d10b
Wiliam102 <ribeirowiliam562@gmail.com>
2026-02-04 19:48:00 -0300

    adicionando secao a tela de inicio

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,7 +1,8 @@
 import BannerInicio from '../../assets/img-tela-inicial.png'
+import styles from './Inicio.module.scss'
 function Inicio(){
     return(
-        <section>
+        <section className={styles.secaoInicio}>
             <div>
                 <h1>Projetos Sociais que transformam</h1>
                <p>Conectamos sua empresa com projetos sociais impactantes. Juntos, criamos mudanças reais na comunidade.</p>
